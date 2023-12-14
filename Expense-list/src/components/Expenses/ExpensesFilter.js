01922b8a9c495f701987ae42c7421f4f4780c6c1
gireesh <pareekg008@gmail.com>
2023-12-14 12:59:18 +0530

    changed years

--- Expense-list/src/components/Expenses/ExpensesFilter.js
+++ Expense-list/src/components/Expenses/ExpensesFilter.js
@@ -14,10 +14,10 @@ const ExpensesFilter = (props) => {
         <label>Filter by year</label>
         <select value={props.selected} onChange={yearSelectionHandler}>
           <option value="year">Select Year</option>
+          <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
-          <option value="2019">2019</option>
         </select>
       </div>
     </div>
